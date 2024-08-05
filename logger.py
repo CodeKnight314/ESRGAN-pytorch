ae80c3880698791c7e98cdfaaad0726bdadbe86f
CodeKnight314 <rtang24@nmhschool.org>
2024-08-05 13:51:23 -0700

    Change logger

--- logger.py
+++ logger.py
@@ -7,8 +7,7 @@ class LOGWRITER:
     def __init__(self, output_directory: str, total_epochs: int):
         self.output_dir = output_directory
         self.total_epochs = total_epochs
-        log_files_count = len(glob(os.path.join(output_directory, "*.txt")))
-        self.output_file_dir = os.path.join(self.output_dir, f"Log_{log_files_count}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
+        self.output_file_dir = os.path.join(self.output_dir, f"Log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
         self._create_log_file()
     
     def _create_log_file(self):
