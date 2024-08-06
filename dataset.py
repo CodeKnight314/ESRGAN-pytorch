1e65525a73b35842b982f25a929ed354414ed31b
CodeKnight314 <rtang24@nmhschool.org>
2024-08-05 22:01:33 -0700

    Modify ImageDataset

--- dataset.py
+++ dataset.py
@@ -8,18 +8,22 @@ import os
 class ImageDataset(Dataset):
     """
     Args:
-        clean_dir (str): The directory containing the clean images.
+        root_dir (str): The directory containing the images.
         patch_size (int): The patch size for a patch_size by patch_size patch.
         scale_factor (float): The factor by which to downscale the images for degradation.
+        mode (str): The mode indicating whether the dataset is for training or validation.
     """
-    def __init__(self, root_dir, patch_size, scale_factor, mode : str ):
+    def __init__(self, root_dir, patch_size, scale_factor, mode: str):
         self.clean_dir = sorted(glob(os.path.join(root_dir, mode) + "/*"))
         self.patch_size = patch_size
         self.scale_factor = scale_factor
 
-        self.transformer = T.Compose([
+        self.hr_transformer = T.Compose([
             T.ToTensor(),
+            T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
         ])
+
+        self.lr_transformer = T.ToTensor()
 
         self.to_pil = T.ToPILImage()
 
@@ -27,9 +31,9 @@ class ImageDataset(Dataset):
         return len(self.clean_dir)
 
     def __getitem__(self, index):
-        clean_img = self.transformer(Image.open(self.clean_dir[index]).convert("RGB"))
+        clean_img = Image.open(self.clean_dir[index]).convert("RGB")
 
-        img_w, img_h = clean_img.shape[1], clean_img.shape[2]
+        img_w, img_h = clean_img.size
 
         if self.patch_size > img_w or self.patch_size > img_h:
             raise ValueError("[INFO] Patch size is larger than the image dimensions")
@@ -37,11 +41,15 @@ class ImageDataset(Dataset):
         start_x = random.randint(0, img_w - self.patch_size)
         start_y = random.randint(0, img_h - self.patch_size)
 
-        clean_img_patch = clean_img[:, start_x:(start_x + self.patch_size), start_y:(start_y + self.patch_size)]
+        clean_img_patch = clean_img.crop((start_x, start_y, start_x + self.patch_size, start_y + self.patch_size))
 
-        lr_img_patch = self.to_pil(clean_img_patch)
-        lr_img_patch = lr_img_patch.resize((int(self.patch_size / self.scale_factor), int(self.patch_size / self.scale_factor)), Image.BICUBIC)
-        lr_img_patch = self.transformer(lr_img_patch)
+        lr_img_patch = clean_img_patch.resize(
+            (int(self.patch_size / self.scale_factor), int(self.patch_size / self.scale_factor)),
+            Image.BICUBIC
+        )
+        
+        clean_img_patch = self.hr_transformer(clean_img_patch)
+        lr_img_patch = self.lr_transformer(lr_img_patch)
 
         if random.random() > 0.5:
             clean_img_patch = T.functional.hflip(clean_img_patch)
@@ -53,6 +61,6 @@ class ImageDataset(Dataset):
 
         return clean_img_patch, lr_img_patch
 
-def load_dataset(root_dir : str, mode : str, batch_size : int = 32, patch_size : int = 128, scale_factor : int = 4):
+def load_dataset(root_dir: str, mode: str, batch_size: int = 32, patch_size: int = 128, scale_factor: int = 4):
     ds = ImageDataset(root_dir=root_dir, mode=mode, patch_size=patch_size, scale_factor=scale_factor)
-    return DataLoader(dataset=ds, batch_size=batch_size, shuffle=True, num_workers=4)
+    return DataLoader(dataset=ds, batch_size=batch_size, shuffle=True, num_workers=8)
